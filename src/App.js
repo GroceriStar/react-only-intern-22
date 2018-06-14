45b06fdd08746b60558ea77ad9548f0cce8d8fb7
Junior Gantin <nioperas06@gmail.com>
2018-06-14 17:59:08 +0100

    Add part2 code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    getDayName(number) {
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[number];
+    }
+
     render() {
-        return ( 
-		<div> Hello world! </div>
-        );
+        let today = new Date().getDay();
+        if (today === 6 || today === 0)
+            return <div>Today is { this.getDayName(today) } - You should go to work today</div>;
+        return <div>Today is { this.getDayName(today) } - You can watch TV all day today</div>;
     }
 }
 
