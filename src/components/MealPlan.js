63471d5a6d5a8c155450679fd3177d86068e10a5
Junior Gantin <nioperas06@gmail.com>
2018-06-21 07:38:00 +0100

    Part 5

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 
 class MealPlan extends Component {
-    state = {
+    state =  {
         meal: '',
-        meals: []
+        meals: ['Pizza']
     };
-
     renderMeals() {
         return _.map(this.state.meals, meal => <li>{meal}</li>);
     }
@@ -15,10 +14,10 @@ class MealPlan extends Component {
         return(
             <div>
                 <h2>Today, you should eat this:</h2>
-                <input type="text" value={this.state.meal} onChange={e => this.setState({meal: e.target.value})} />
-                <button onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})} >Add Meal</button>
+                <input type="text" onChange={e => this.setState({meal: e.target.value})} value={this.state.meal}/>
+                <button onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})}>Add Meal</button>
                 <ul>
-                    { this.renderMeals() }
+                    {this.renderMeals()}
                 </ul>
             </div>
         );
