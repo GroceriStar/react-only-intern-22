b7f8fce6c397ab293f69d9a3ad5cae3f8571c876
Junior Gantin <nioperas06@gmail.com>
2018-07-10 00:55:01 +0100

    (build) deploy to Netlify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Template from './layouts';
 
-ReactDOM.render( <App/> , document.getElementById('root'));
+ReactDOM.render( <Template/> , document.getElementById('root'));
 registerServiceWorker();
